e519529f83891e946851af0c967016f1387b6f90
tatur24 <54002798+tatur24@users.noreply.github.com>
2020-04-12 13:51:13 -0500

    class 3, working chat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,27 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+var fbConfig = {
+  apiKey: "AIzaSyBmp1d_-QechGk8JY2vHoMdRZewKVM006Q",
+  authDomain: "fsdi-6aa30.firebaseapp.com",
+  databaseURL: "https://fsdi-6aa30.firebaseio.com",
+  projectId: "fsdi-6aa30",
+  storageBucket: "fsdi-6aa30.appspot.com",
+  messagingSenderId: "92558793025"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(fbConfig),
+    AngularFirestoreModule,
+  ],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { DataService } from '../services/data.service';
+import { Message } from '../models/message';
+
 
 @Component({
   selector: 'app-tab1',
@@ -8,13 +10,37 @@ import { DataService } from '../services/data.service';
 })
 export class Tab1Page {
 
+  displayMessage: Message[];
 
   constructor(private data: DataService) {
+   //this.homework();
+    data.getAllMessages().subscribe(list => {
+      
+      this.displayMessage = list.sort((left, right) => {
+          // return -1 when left should go first
+          // return 1 when right should go first
+          // return 0 if they are the same
+          if(!left.createdOn) return -1; // if left don't have a date, it goes first
+
+          if(left.createdOn > right.createdOn){
+            return 1;
+          }
+          else if(right.createdOn > left.createdOn){
+            return -1;
+          }
+
+          return 0;
+
+      })
+    });
+  }
 
 
   }
-getMessages(){
-  console.log(this.data.getAllMessages());
-  return this.data.getAllMessages();
-}
-}
+
+
+
+// 1 - Sort items by Age Desc
+    // 2 - Sort items by Age Asc
+    // 3 - Print only actives
+    // 4 - Sum all the balances
